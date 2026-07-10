95484873e749e90ba54f42fae0b1a744bb24b514
junjie <114612143+junjie-shentu@users.noreply.github.com>
2026-07-10 20:13:49 +0100

    Update script

--- generate_lesion_mask_pair.sh
+++ generate_lesion_mask_pair.sh
@@ -1,4 +1,4 @@
-python run_sd_boxdiff_skin_lesion_semantic_v5.py \
+python run_sd_LesionMaskDiffPipeline.py \
     --prompt "a photo of <lesion> on <skin>" \
     --pretrained_unet_path /directory/to/pretrained/unet/model \
     --semantic_mask_folder /directory/to/semantic/mask/folder
